299efed877f98bac0aaf6fb363c28362665d50c6
Eros <george.marr.cobc@gmail.com>
2018-10-06 11:22:28 +0100

    error handling & debug, also main is there so clion can stfu

--- mole.c
+++ mole.c
@@ -1,3 +1,26 @@
 #include "mole.h"
 #include <stdio.h>
+#include <stdbool.h>
 #define MOLE_LIBRARY_H
+
+bool debug = true;
+
+void handle_debug(char *msg, ...){
+    va_list args;
+    if(debug){
+        va_start(args, msg);
+        vprintf(msg, args);
+        va_end(args);
+    }
+}
+
+void handle_errors(char *msg, ...){
+    va_list args;
+    va_start(args, msg);
+    vprintf(msg, args);
+    va_end(args);
+}
+
+int main(){
+    return 0;
+}
