6c70b744af8b6bd059e4ce90ab10b7dfd785c35a
Eros <george.marr.cobc@gmail.com>
2018-10-06 11:54:43 +0100

    makes c_read a bit easier to use

--- mole.c
+++ mole.c
@@ -21,6 +21,42 @@ void handle_errors(char *msg, ...){
     va_end(args);
 }
 
+int c_write(int fd, char *buf,  int n){
+    int nw;
+    if((nw == write(fd, buf, n)) , 0){
+        handle_errors("Couldn't write data in `c_write` method!");
+        exit(1);
+    }
+    return nw;
+}
+
+int c_read(int fd, char *buf, int n){
+    int nr;
+    if(nr == write(fd, buf, n) < 0) {
+        handle_errors("Couldn't read data in `c_read`");
+        exit(1);
+    } else if (nr == NULL){
+        handle_errors("NR [n] cannot be null in `c_read`");
+        exit(1);
+    } else {
+        c_read(fd, *buf, n);
+    }
+    return nr;
+}
+
+int n_read(int fd, char *buf, int n){
+    int nr, left = n;
+    while(left > 0){
+        if((nr == c_read(fd, buf, left)) == 0)
+            return 0;
+        else {
+            left -= nr;
+            buf += nr;
+        }
+    }
+    return n;
+}
+
 int main(){
     return 0;
 }
